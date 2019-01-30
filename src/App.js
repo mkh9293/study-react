b7495316ed051a894fabf54b8453b16b01ed03be
문광훈 <gwanghun.moon@hancom.com>
2019-01-30 18:32:31 +0900

    add Form, TodoListTemplate Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import TodoListTemaplate from './components/TodoListTemplate';
+import Form from'./components/Form';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
+      <TodoListTemaplate form={<Form/>}>
+        todo list template
+      </TodoListTemaplate>
     );
   }
 }
